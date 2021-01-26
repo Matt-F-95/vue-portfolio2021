784a8e2a0b05225b43973c5de99b7d4109db76fc
Matt Felton <matthew.felton@senecacollege.ca>
2021-01-26 13:21:36 -0500

    Modified about me, added styling to portfolio work section component

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -5,15 +5,18 @@
     <div class="col s12 m4 s1">
       <div class="card">
         <div class="card-image">
-          <img src="http://placekitten.com/g/200/200">
+          <img src="@/assets/foe-app-cs.png">
           <span class="card-title">Family of Eateries</span>
         </div>
         <div class="card-content">
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
+          <p>An App made and designed for Family of Eateries.</p>
+          <i class="fab fa-html5"></i>
+          <i class="fab fa-css3"></i>
+          <i class="fab fa-js"></i>
         </div>
         <div class="card-action">
-          <a href="#">This is a link</a>
+          <a href="#">Case Study</a>
+          <a href="#"><i class="fab fa-github"></i></a>
         </div>
       </div>
     </div>
@@ -28,7 +31,8 @@
           I am convenient because I require little markup to use effectively.</p>
         </div>
         <div class="card-action">
-          <a href="#">This is a link</a>
+           <a href="#">Case Study</a>
+          <a href="#"><i class="fab fa-github"></i></a>
         </div>
       </div>
     </div>
@@ -43,7 +47,8 @@
           I am convenient because I require little markup to use effectively.</p>
         </div>
         <div class="card-action">
-          <a href="#">This is a link</a>
+          <a href="#">Case Study</a>
+          <a href="#"><i class="fab fa-github"></i></a>
         </div>
       </div>
     </div>
@@ -60,7 +65,8 @@
           I am convenient because I require little markup to use effectively.</p>
         </div>
         <div class="card-action">
-          <a href="#">This is a link</a>
+           <a href="#">Case Study</a>
+          <a href="#"><i class="fab fa-github"></i></a>
         </div>
       </div>
     </div>
@@ -75,7 +81,8 @@
           I am convenient because I require little markup to use effectively.</p>
         </div>
         <div class="card-action">
-          <a href="#">This is a link</a>
+           <a href="#">Case Study</a>
+          <a href="#"><i class="fab fa-github"></i></a>
         </div>
       </div>
     </div>
@@ -90,7 +97,8 @@
           I am convenient because I require little markup to use effectively.</p>
         </div>
         <div class="card-action">
-          <a href="#">This is a link</a>
+          <a href="#">Case Study</a>
+          <a href="#"><i class="fab fa-github"></i></a>
         </div>
       </div>
     </div>
@@ -107,7 +115,8 @@
           I am convenient because I require little markup to use effectively.</p>
         </div>
         <div class="card-action">
-          <a href="#">This is a link</a>
+           <a href="#">Case Study</a>
+          <a href="#"><i class="fab fa-github"></i></a>
         </div>
       </div>
     </div>
@@ -143,6 +152,32 @@
 </script>
 
 
-<style scoped>
+<style>
+  .card-content {
+    background: #252934;
+    color: #fff;
+  }
+
+  .card-action {
+    background: #1B232E !important;
+    color: #fff;
+    font-size: 1.25rem;
+    text-align: center;
+    
+  }
+
+  .card-content i {
+    font-size: 22px;
+    margin: 2px 2px 2px 2px;
+  }
+
+  .card-title {
+    background: #252525;
+    width: 100%;
+    text-align: center;
+    height: 10vh;
+  }
+
+
 
 </style>
